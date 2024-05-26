becaf4b30d436ab408318001b66dd5ef2f24d442
Big_Cake <me@lihaoyu.cn>
2024-05-26 18:45:33 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,3 @@
-<script setup lang="ts">
-import { setColorScheme } from 'mdui/functions/setColorScheme.js';
-
-setColorScheme('#5cabff');
-</script>
-
 <template>
   <Navbar />
   <div class="m-auto p-4">
