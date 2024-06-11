4b6e4c8313dac95b614c1a6e903de067c816312b
FalfaChino <falfachino@qq.com>
2024-06-11 19:03:42 +0800

    ✨ upd: notice

--- src/pages/favorability.vue
+++ src/pages/favorability.vue
@@ -5,12 +5,13 @@
         <blockquote>字如其意，看看你和哪个学生最亲密呢？<br /><del>我不管 爱丽丝万岁~\(≧▽≦)/~</del></blockquote>
         <p>可以在这里查看各个学生的好感度以及所有用户的好感度排名……</p>
         <p>如果没有登录，默认显示全局排行榜；若登录了账号，那么此处将默认只显示自己的好感度排行榜。</p>
+        <p>登录之后才可按 UID 和角色名查询数据。</p>
         <blockquote>别催了别催了，在写了，先让我急（（（</blockquote>
         <blockquote>登录相关的东西还没做，还有些小问题，不过最起码能查询全局排行榜了。</blockquote>
         <mdui-divider></mdui-divider>
         <RankTable />
     </div>
-    
+
 </template>
 
 <script setup lang="ts">
